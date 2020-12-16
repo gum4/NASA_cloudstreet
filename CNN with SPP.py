a41f4ef7cb0838998c0db61b370b6ef200cc519c
NeverThinkOfLWJ <46687992+gum4@users.noreply.github.com>
2020-12-16 01:55:38 -0500

    Add files via upload

---
+++ CNN with SPP.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 01:52:33 2020
+
+@author: gumenghan
+"""
+
+import math
+import torch
+import torch.nn.functional as F
+
+class SPPLayer(torch.nn.Module):
+
+    def __init__(self, num_levels, pool_type='max_pool'):
+        super(SPPLayer, self).__init__()
+
+        self.num_levels = num_levels
+        self.pool_type = pool_type
+
+    def forward(self, x):
+        num, c, h, w = x.size() 
+        for i in range(self.num_levels):
+            level = i+1
+            kernel_size = (math.ceil(h / level), math.ceil(w / level))
+            stride = (math.ceil(h / level), math.ceil(w / level))
+            pooling = (math.floor((kernel_size[0]*level-h+1)/2), math.floor((kernel_size[1]*level-w+1)/2))
+
+            if self.pool_type == 'max_pool':
+                tensor = F.max_pool2d(x, kernel_size=kernel_size, stride=stride, padding=pooling).view(num, -1)
+            else:
+                tensor = F.avg_pool2d(x, kernel_size=kernel_size, stride=stride, padding=pooling).view(num, -1)
+
+            if (i == 0):
+                x_flatten = tensor.view(num, -1)
+            else:
+                x_flatten = torch.cat((x_flatten, tensor.view(num, -1)), 1)
+        return x_flatten
